18bfddc2e030dcda42a1d22717d65ca4952b2c90
Ali T <aliteeney@googlemail.com>
2017-05-11 23:13:10 +0100

    Remove unnecessary whitespace

--- tests/test_edge.py
+++ tests/test_edge.py
@@ -6,21 +6,20 @@ from startracker.star import Star
 from startracker.edge import Edge
 
 class Edge_test(unittest.TestCase):
-    
-    
+
     def test_equal_self(self):
         s1 = Star(1,0,None,None)
         s2 = Star(2,0,None,None)
         e = Edge(s1,s2)
         self.assertEquals(e,e)
-    
+
     def test_equal_duplicate(self):
         s1 = Star(1,0,None,None)
         s2 = Star(2,0,None,None)
         e1 = Edge(s1,s2)
         e2 = Edge(s1,s2)
         self.assertEquals(e1,e2)
-       
+
     def test_unequals(self):
         s1 = Star(1,0,None,None)
         s2 = Star(2,0,None,None)
